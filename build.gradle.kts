20c14474fade4e2edb3840ae57e3a1a65714468b
cjpoulsen <53880342+cjpoulsen@users.noreply.github.com>
2022-03-26 14:36:53 -0500

    Added Gradle.

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+  kotlin("jvm") version "1.6.10" apply false
+}
+
+allprojects {
+  version = "0.0.1"
+  group = "org.merideum.kotlin"
+}
+
+repositories {
+  mavenCentral()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "merideum-kotlin"
+
